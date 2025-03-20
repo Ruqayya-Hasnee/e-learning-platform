b20757e5fb6d8c192dbb1cf63aef378766dfba58
Sameea Adnan <sameea.saqee@gmail.com>
2025-03-20 23:03:36 +0500

    change backgroung image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-full m-0 p-0">
-      <div className="bg-[url('/landing-page.avif')] bg-cover bg-no-repeat min-h-screen overflow-hidden">
+      <div className="bg-[url('/landing-page-bg.jpg')] bg-cover bg-no-repeat min-h-screen overflow-hidden">
         <div className="flex justify-between items-center h-12 mt-32 mx-35">
           <div className="w-lg">
             <h1 className="text-5xl font-bold text-blue-900">E-learning</h1>
