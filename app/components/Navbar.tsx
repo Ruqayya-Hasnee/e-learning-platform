4f8e3393cca545f3f7d4aaf502aa2f3a8321bd84
Sameea Adnan <sameea.saqee@gmail.com>
2025-03-21 21:36:07 +0500

    updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,45 +7,46 @@ import { RoleType } from "@/types/user";
 
 function Navbar() {
   const { user, logout } = useAuth();
-
   return (
-    <div className="text-blue-900 flex justify-between items-center h-12 mx-35">
-      <div className="font-bold">
-        <Link href="/">E-Learning</Link>
-      </div>
-      <div className="flex gap-12 font-bold">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/courses">Courses</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
+    <div>
+      <div className="text-blue-900 flex justify-between items-center h-12 mx-35 ">
+        <div className="font-bold">
+          <Link href="/">E-Learning</Link>
+        </div>
+        <div className="flex gap-12 font-bold">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/courses">Courses</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
 
-      <div className="flex gap-4">
-        {user ? (
-          <>
-            <Link
-              href={
-                user?.role === RoleType.STUDENT
-                  ? "/studentprofile"
-                  : "/instructorprofile"
-              }
-            >
-              Profile
-            </Link>
-            <button className="primary" onClick={logout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link href="/signup">
-              <button className="secondary">Sign Up</button>
-            </Link>
-            <Link href="/login">
-              <button className="secondary">Login</button>
-            </Link>
-          </>
-        )}
+        <div className="flex gap-4">
+          {user ? (
+            <>
+              <Link
+                href={
+                  user?.role === RoleType.STUDENT
+                    ? "/studentprofile"
+                    : "/instructorprofile"
+                }
+              >
+                Profile
+              </Link>
+              <button className="primary" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/signup">
+                <button className="secondary">Sign Up</button>
+              </Link>
+              <Link href="/login">
+                <button className="secondary">Login</button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
